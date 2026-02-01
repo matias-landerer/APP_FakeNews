604e5d03ed9a44d5d52c0d028257293d7a54c3f4
matias-landerer <lanconts7@gmail.com>
2026-01-31 23:48:21 -0300

    Avance pagina registro flutter

--- Client/client_flutter/lib/register_page.dart
+++ Client/client_flutter/lib/register_page.dart
@@ -2,31 +2,34 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({super.key});
 
   @override
-  State<LoginPage> createState() => _LoginPageState();
+  State<RegisterPage> createState() => _RegisterPageState();
 }
 
-class _LoginPageState extends State<LoginPage> {
+class _RegisterPageState extends State<RegisterPage> {
   final userController = TextEditingController();
+  final emailController = TextEditingController();
   final passController = TextEditingController();
+  final pass2Controller = TextEditingController();
 
   String error = "";
   bool loading = false;
 
-  Future<void> login() async {
+  Future<void> register() async {
     setState(() {
       loading = true;
       error = "";
     });
 
     final response = await http.post(
-      Uri.parse("http://10.0.2.2:5000/login"),
+      Uri.parse("http://10.0.2.2:5000/register"),
       headers: {"Content-Type": "application/json"},
       body: jsonEncode({
         "username": userController.text,
+        "email": emailController.text,
         "password": passController.text,
       }),
     );
@@ -37,13 +40,9 @@ class _LoginPageState extends State<LoginPage> {
       loading = false;
     });
 
-    if (data["status"] == "InicioExitoso") {
+    if (data["status"] == "RegistroExitoso") {
       // Login exitoso → ir a Home
       Navigator.pushReplacementNamed(context, "/home");
-    }
-    
-    else if  (data["status"] == "UsuarioNoExiste") {
-      Navigator.pushReplacementNamed(context, "/register");
     }
     
     else {
@@ -66,15 +65,24 @@ class _LoginPageState extends State<LoginPage> {
               decoration: const InputDecoration(labelText: "Usuario"),
             ),
             TextField(
+              controller: emailController,
+              decoration: const InputDecoration(labelText: "Email"),
+            ),
+            TextField(
               controller: passController,
               decoration: const InputDecoration(labelText: "Contraseña"),
+              obscureText: true,
+            ),
+            TextField(
+              controller: pass2Controller,
+              decoration: const InputDecoration(labelText: "Confirme Contraseña"),
               obscureText: true,
             ),
             const SizedBox(height: 20),
             if (loading) const CircularProgressIndicator(),
             if (!loading)
               ElevatedButton(
-                onPressed: login,
+                onPressed: register,
                 child: const Text("Entrar"),
               ),
             const SizedBox(height: 10),
