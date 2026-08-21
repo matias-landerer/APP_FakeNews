e97890de410233b1498bb05f9c5ee354a244e609
matias-landerer <lanconts7@gmail.com>
2026-08-21 17:31:26 -0400

    fix: Mejora en la redacción y ortografía de user-info.

--- Client/client_flutter/lib/info.dart
+++ Client/client_flutter/lib/info.dart
@@ -57,17 +57,16 @@ class InfoPage extends StatelessWidget {
                         "una descripción de por qué se llegó a esa conclusión y una listas con las fuentes que se usaron.\n"
                         "CONSIDERACIONES IMPORTANTES:\n- El titular debe ser descriptivo. Hay muchas noticias con titulares muy vagos. Por ejemplo,"
                         " un reportaje que estudia el indice de felicidad en Chile podría llamarse \"¿Qué tan felices somos los chilenos?\" "
-                        "Lo cuál claramente no es suficiente para determinar la veracidad. Titulares como \"Presidente anuncia nueva ley\" también son vagos,"
-                        " pues no se aclara el país ni la ley en específico (la app no asume que se trata de Chile o cualquier otro sitio).\nPero cualquier"
-                        " oración que corresponda a una afirmación clara servirá como un titular para ser analizado.\n- El análisis de veracidad es hecho por"
-                        " una IA, por lo que es importante siempre revisar las fuentes entregadas en lugar de creer en el resultado ciegamente.\n- Esta app"
-                        " es un detector de noticias falsas, NO ES UN LLM, por lo que la manera correcta de usarla es introducir un titular de noticia o un"
-                        " hecho leido en redes sociales con el fin de saber si de verdad ocurrió. No funcionará bien si se le hacen preguntas, ni tampoco"
-                        " reaccionará si se le pide que algo algo que no sea analizar un titular.\nSISTEMA DE CRÉDITOS:\nCada análisis gasta un crédito, y "
-                        "para conseguir más se compra un paquete. Al crear una cuenta se incluyen 20 créditos gratuitos.\nRecomendación: probar bien la app"
-                        " con esos 20 créditos y si la app resulta útil, evaluar si vale la pena obtener más.\nPor favor cualquier tipo de error encontrado"
-                        " en la app (ya sea en la verificación del titular, en la interfáz o en la conexión al servidor) comunicar a"
-                        " fake.news.detector.support@gmail.comMUCHAS GRACIAS POR USAR LA APP :)",
+                        "Lo cuál claramente no es suficiente para determinar su veracidad. Titulares como \"Presidente anuncia nueva ley\" también son vagos,"
+                        " pues no se aclara el país ni la ley en específico (la app no asume que se trata de Chile o cualquier otro sitio).\n- El análisis"
+                        " de veracidad es hecho por una IA, por lo que es importante siempre revisar las fuentes entregadas en lugar de creer en el resultado"
+                        " ciegamente.\n- Esta app es un detector de noticias falsas, NO ES UN LLM, por lo que la manera correcta de usarla es introducir un"
+                        " titular de noticia o un acontecimiento leido en redes sociales con el fin de saber si en verdad ocurrió. No funcionará bien si se"
+                        " le hacen preguntas, ni tampoco reaccionará si se le pide que haga algo que no sea analizar un titular.\nSISTEMA DE CRÉDITOS:\nCada"
+                        " análisis gasta un crédito, y para conseguir más se compra un paquete. Al crear una cuenta se incluyen 20 créditos gratuitos."
+                        "\nRecomendación: probar bien la app con esos 20 créditos y si la app resulta útil, evaluar si vale la pena obtener más.\n\nPor favor,"
+                        " cualquier tipo de error encontrado en la app (ya sea en la verificación del titular, en la interfaz o en la conexión al servidor)"
+                        " comunicar a fake.news.detector.support@gmail.com\nMUCHAS GRACIAS POR USAR LA APP :)",
                         style: TextStyle(
                           fontSize: 16,
                           fontWeight: FontWeight.w600,
